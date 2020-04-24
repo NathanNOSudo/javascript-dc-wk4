358fbaf7df14cb2e2746dcb1726c7a2d0e4b55db
Nathan <nathandavid719@gmail.com>
2020-04-24 09:50:18 -0500

    counter from live share added

---
+++ js/counter.js
@@ -0,0 +1,35 @@
+// counterTextBox
+// startCounterButton
+
+let counterTextBox = document.getElementById("counterTextBox")
+let startCounterButton = document.getElementById("startCounterButton")
+
+let counterHeading = document.getElementById("counterHeading")
+
+startCounterButton.addEventListener("click", function() {
+
+    let value = counterTextBox.value
+    console.log(value)
+
+    // start the count down timer using set interval 
+   let id = setInterval(function() {
+        console.log("tick")
+
+        // change the background of the body to green 
+        document.body.style.background = "green"
+
+        // display the count down on the screen 
+        counterHeading.innerHTML = value
+        // assign the new value subtracting one
+        value = value - 1;
+
+        if (value < 0) {
+            document.body.style.background = "red"
+            // Stop the interval
+            console.log("LESS THEN ZERO")
+        }
+
+
+    }, 1000);
+
+})
